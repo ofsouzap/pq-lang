99efa16a7f2f15f2359c3da6df8b2045fb885977
Ofsouzap <ofsouzap@gmail.com>
2024-10-11 18:09:57 +0100

    Start parser tests

--- lib/ast.ml
+++ lib/ast.ml
@@ -2,3 +2,8 @@ type expr =
   | IntLit of int
   | Add of expr * expr
   | Mult of expr * expr
+
+let rec show = function
+  | IntLit i -> string_of_int i
+  | Add (e1, e2) -> Printf.sprintf "(%s + %s)" (show e1) (show e2)
+  | Mult (e1, e2) -> Printf.sprintf "(%s * %s)" (show e1) (show e2)
--- test/test_pq_lang.ml
+++ test/test_pq_lang.ml
@@ -4,6 +4,7 @@ let () =
   let suite =
     "PQ Lang Tests" >::: [
       Test_lexer.suite;
+      Test_parser.suite;
     ]
   in
   run_test_tt_main suite
