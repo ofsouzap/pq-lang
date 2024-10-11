de5e699de80697c51bc1520d175a1d134fef4a21
Ofsouzap <ofsouzap@gmail.com>
2024-10-11 13:02:14 +0100

    Basic arithmetic lexing, parsing, evaluating

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+open Pq_lang
+
+let rec eval expr =
+  let open Ast in
+  match expr with
+  | IntLit n -> n
+  | Add (e1, e2) -> eval e1 + eval e2
+  | Mult (e1, e2) -> eval e1 * eval e2
+
+let () =
+  let lexbuf = Lexing.from_channel stdin in
+  let ast = Parser.prog Lexer.token lexbuf in
+  let result = eval ast in
+  Printf.printf "%d\n" result
