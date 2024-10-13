2b0bf53e8e1f3e39f37dd021f2fbd74c1eec3e9f
Ofsouzap <ofsouzap@gmail.com>
2024-10-13 16:36:56 +0100

    AST executor store implementation property-based tests

--- test/test_pq_lang.ml
+++ test/test_pq_lang.ml
@@ -2,10 +2,12 @@ open OUnit2
 
 let () =
   let suite =
-    "PQ Lang Tests" >::: [
-      Test_lexer.suite;
-      Test_parser.suite;
-      Test_ast_executor.suite;
-    ]
+    "PQ Lang Tests"
+    >::: [
+           Test_lexer.suite;
+           Test_parser.suite;
+           Test_ast_executor.suite;
+           Test_ast_executor_store.suite;
+         ]
   in
   run_test_tt_main suite
