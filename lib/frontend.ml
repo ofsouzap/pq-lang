f4bff06dd3281e8b06f35704d8ff89b085b2799e
Ofsouzap <ofsouzap@gmail.com>
2024-10-22 12:10:12 +0100

    Fix function types were not usable

--- lib/frontend.ml
+++ lib/frontend.ml
@@ -1,9 +1,12 @@
 type run_frontend_res = Res of Ast.expr | LexingError of char | ParsingError
 
-let run_frontend_string (input : string) : run_frontend_res =
-  try
-    let lexbuf = Lexing.from_string input in
-    Res (Parser.prog Lexer.token lexbuf)
-  with
+let lex_parse_from_lexbuf (lexbuf : Lexing.lexbuf) : run_frontend_res =
+  try Res (Parser.prog Lexer.token lexbuf) with
   | Lexer.LexingError c -> LexingError c
   | Parser.Error -> ParsingError
+
+let run_frontend_channel (input : in_channel) : run_frontend_res =
+  lex_parse_from_lexbuf (Lexing.from_channel input)
+
+let run_frontend_string (input : string) : run_frontend_res =
+  lex_parse_from_lexbuf (Lexing.from_string input)
--- lib/frontend.mli
+++ lib/frontend.mli
@@ -5,5 +5,8 @@ type run_frontend_res =
       (** A lexing error occured on the specified character *)
   | ParsingError  (** A parsing error occured on the token stream *)
 
+(** Run the lexer and parser on an input channel *)
+val run_frontend_channel : in_channel -> run_frontend_res
+
 (** Run the lexer and parser on a string input *)
 val run_frontend_string : string -> run_frontend_res
