ede934e26f959b0f623a2fc99d8b5b9dc6ec9d3b
Ofsouzap <ofsouzap@gmail.com>
2024-11-04 00:56:44 +0000

    Open core in frontend file and main file

--- lib/frontend.mli
+++ lib/frontend.mli
@@ -1,3 +1,5 @@
+open Core
+
 (** A result from trying to run the lexer and parser on an input *)
 type run_frontend_res =
   | Res of Ast.expr  (** A successful lex and parse, with the resulting AST *)
@@ -6,7 +8,7 @@ type run_frontend_res =
   | ParsingError  (** A parsing error occured on the token stream *)
 
 (** Run the lexer and parser on an input channel *)
-val run_frontend_channel : in_channel -> run_frontend_res
+val run_frontend_channel : In_channel.t -> run_frontend_res
 
 (** Run the lexer and parser on a string input *)
 val run_frontend_string : string -> run_frontend_res
