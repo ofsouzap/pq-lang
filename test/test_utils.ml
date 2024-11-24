031b1d0d1d5779823240a114c9d0067acff394e1
Ofsouzap <ofsouzap@gmail.com>
2024-11-24 21:20:28 +0000

    More ast printing fixes

--- test/test_utils.ml
+++ test/test_utils.ml
@@ -3,6 +3,7 @@ open OUnit2
 open QCheck
 open Pq_lang
 open Vtype
+open Ast
 open Utils
 
 let create_typed_expr_gen_test_for_base_type (name : string) (t : vtype) : test
@@ -22,19 +23,12 @@ let create_typed_expr_gen_test_for_compound_type (name : string)
   QCheck_runner.to_ounit2_test
     (QCheck.Test.make ~name ~count:1000
        (let open QCheck.Gen in
-        let t_arb =
-          make ~print:(Core.Fn.compose Sexp.to_string sexp_of_vtype) t_gen
-        in
+        let t_arb = make ~print:vtype_to_source_code t_gen in
         let e_gen =
           t_gen >>= fun t ->
           QCheck.gen (ast_expr_arb ~t PrintExprSource Gen.unit)
         in
-        let e_arb =
-          make
-            ~print:
-              (Core.Fn.compose Sexp.to_string (Ast.sexp_of_expr sexp_of_unit))
-            e_gen
-        in
+        let e_arb = make ~print:ast_to_source_code e_gen in
         QCheck.pair t_arb e_arb)
        (fun (t, e) ->
          let typed_result = Typing.type_expr e in
