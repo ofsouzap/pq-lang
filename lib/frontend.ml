2f9d81e15bdb7e8bab19cc7b6c17510eafb5da72
Ofsouzap <ofsouzap@gmail.com>
2024-12-20 23:39:16 +0000

    Use Core.Result module for frontend

--- lib/frontend.ml
+++ lib/frontend.ml
@@ -1,14 +1,12 @@
 open Core
 
-type run_frontend_res =
-  | Res of Ast.plain_expr
-  | LexingError of char
-  | ParsingError
+type frontend_error = LexingError of char | ParsingError
+type run_frontend_res = (Ast.plain_expr, frontend_error) Result.t
 
 let lex_parse_from_lexbuf (lexbuf : Lexing.lexbuf) : run_frontend_res =
-  try Res (Parser.prog Lexer.token lexbuf) with
-  | Lexer.LexingError c -> LexingError c
-  | Parser.Error | Parsing_errors.CustomError -> ParsingError
+  try Ok (Parser.prog Lexer.token lexbuf) with
+  | Lexer.LexingError c -> Error (LexingError c)
+  | Parser.Error | Parsing_errors.CustomError -> Error ParsingError
 
 let run_frontend_channel (input : In_channel.t) : run_frontend_res =
   lex_parse_from_lexbuf (Lexing.from_channel input)
