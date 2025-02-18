a1e95ec279199f3ca898c361510429a6f3b0fe6d
Ofsouzap <ofsouzap@gmail.com>
2025-02-18 17:52:52 +0000

    Tmp change - just test Z3 integration

--- test/test_pq_lang.ml
+++ test/test_pq_lang.ml
@@ -4,7 +4,7 @@ let () =
   let suite =
     "PQ Lang Tests"
     >::: [
-           Test_testing_utils.suite;
+           (* Test_testing_utils.suite;
            Test_utils.suite;
            Test_ast.suite;
            Test_program.suite;
@@ -12,8 +12,8 @@ let () =
            Test_ast_executor.suite;
            Test_ast_executor_store.suite;
            Test_interpreting_integration.suite;
-           Test_typing.suite;
-           (* Test_quotient_type_checking.suite; *)
+           Test_typing.suite; *)
+           Test_quotient_type_checking.suite;
          ]
   in
   run_test_tt_main suite
