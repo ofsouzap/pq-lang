b2bdee28dc3fc962c60a06114cce659d1dd23bd2
Ofsouzap <ofsouzap@gmail.com>
2025-02-19 13:36:12 +0000

    Uncomment main tests

--- test/test_pq_lang.ml
+++ test/test_pq_lang.ml
@@ -4,7 +4,7 @@ let () =
   let suite =
     "PQ Lang Tests"
     >::: [
-           (* Test_testing_utils.suite;
+           Test_testing_utils.suite;
            Test_utils.suite;
            Test_ast.suite;
            Test_program.suite;
@@ -12,7 +12,7 @@ let () =
            Test_ast_executor.suite;
            Test_ast_executor_store.suite;
            Test_interpreting_integration.suite;
-           Test_typing.suite; *)
+           Test_typing.suite;
            Test_quotient_type_checking.suite;
          ]
   in
