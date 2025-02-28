1427e4869e2b0eb50931798ef6191f47e51efc4a
Ofsouzap <ofsouzap@gmail.com>
2025-02-28 23:44:25 +0000

    Add source line number tracking to parser and Frontend module

--- lib/frontend.ml
+++ lib/frontend.ml
@@ -1,15 +1,26 @@
 open Core
+module Program = Program.StdProgram
 
 type frontend_error = LexingError of char | ParsingError
 [@@deriving sexp, equal]
 
-type run_frontend_res = (Program.StdProgram.plain_t, frontend_error) Result.t
+type source_position = { lnum : int; bol : int } [@@deriving sexp, equal]
+
+type run_frontend_res =
+  ((source_position, source_position) Program.t, frontend_error) Result.t
 [@@deriving sexp, equal]
 
 let lex_parse_from_lexbuf (lexbuf : Lexing.lexbuf) : run_frontend_res =
-  try Ok (Parser.prog Lexer.token lexbuf) with
+  let open Result in
+  (try Ok (Parser.prog Lexer.token lexbuf) with
   | Lexer.LexingError c -> Error (LexingError c)
-  | Parser.Error | Parsing_errors.CustomError -> Error ParsingError
+  | Parser.Error | Parsing_errors.CustomError -> Error ParsingError)
+  >>| fun prog ->
+  prog
+  |> Program.fmap_expr ~f:(fun lex_pos ->
+         { lnum = lex_pos.Lexing.pos_lnum; bol = lex_pos.Lexing.pos_bol })
+  |> Program.fmap_pattern ~f:(fun lex_pos ->
+         { lnum = lex_pos.Lexing.pos_lnum; bol = lex_pos.Lexing.pos_bol })
 
 let run_frontend_channel (input : In_channel.t) : run_frontend_res =
   lex_parse_from_lexbuf (Lexing.from_channel input)
