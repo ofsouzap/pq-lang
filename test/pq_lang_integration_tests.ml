961c32db48f7abbca086231e8fb2cd0f6964f39f
Ofsouzap <ofsouzap@gmail.com>
2025-02-28 15:09:09 +0000

    Restructure tests directory to separate unit and integration tests into different folders

--- test/pq_lang_integration_tests.ml
+++
@@ -1,3 +0,0 @@
-let () =
-  Alcotest.run "PQ Lang Integration Tests"
-    [ ("Interpreting", Pq_lang_integration_interpreting.suite) ]
---
+++ test/unit/main.ml
@@ -0,0 +1,13 @@
+let () =
+  Alcotest.run "PQ Lang Unit Tests"
+    [
+      ("Testing Utils", Test_testing_utils.suite);
+      ("Utils", Test_utils.suite);
+      ("Expr", Test_expr.suite);
+      ("Program", Test_program.suite);
+      ("Lexer/parser", Test_lexer_parser.suite);
+      ("Program Executor", Test_programExecutor.suite);
+      ("Program Executor Store", Test_programExecutor_store.suite);
+      ("Type Checker", Test_typeChecker.suite);
+      ("Quotient Type Checker", Test_quotientTypeChecker.suite);
+    ]
