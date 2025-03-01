fc07e27dc14edb58b2a487e9bda119bc3b93dd0c
Ofsouzap <ofsouzap@gmail.com>
2025-03-01 13:00:38 +0000

    Use cmdliner for pq_to_ocaml

--- bin_pq_to_ocaml/main.ml
+++ bin_pq_to_ocaml/main.ml
@@ -1,12 +1,27 @@
 open Core
+open Cmdliner
 open Pq_lang
 module Program = Program.StdProgram
 module QuotientTypeChecker = QuotientTypeChecker.MakeZ3
 module ProgramExecutor = ProgramExecutor.SimpleExecutor
 module TypeChecker = ProgramExecutor.TypeChecker
 
-type exit_code = ExitCode of int
-type error_exit = exit_code * string
+type exit = ErrCallingSystemFunctions | ErrQuotientTypeChecking | ErrInternal
+
+let exit_code : exit -> int = function
+  | ErrCallingSystemFunctions -> 1
+  | ErrQuotientTypeChecking -> 2
+  | ErrInternal -> 3
+
+let exit_info (e : exit) : Cmd.Exit.info =
+  let open Cmd.Exit in
+  (match e with
+  | ErrCallingSystemFunctions -> info ~doc:"Error calling system functions"
+  | ErrQuotientTypeChecking -> info ~doc:"Quotient type checking error"
+  | ErrInternal -> info ~doc:"Internal error")
+    (exit_code e)
+
+type error_exit = exit * string
 
 let pq_filename_to_out_ml_filename (filename : string) : string =
   Filename.chop_extension filename ^ ".ml"
@@ -16,7 +31,7 @@ let pq_filename_to_out_mli_filename (filename : string) : string =
 
 let read_all_file (filename : string) : (string, error_exit) Result.t =
   try Ok (In_channel.read_all filename)
-  with Sys_error msg -> Error (ExitCode 1, msg)
+  with Sys_error msg -> Error (ErrCallingSystemFunctions, msg)
 
 let process_pq_file (filename : string) : (Program.plain_t, error_exit) Result.t
     =
@@ -24,7 +39,7 @@ let process_pq_file (filename : string) : (Program.plain_t, error_exit) Result.t
   read_all_file filename >>= fun contents ->
   Frontend.run_frontend_string contents
   |> Result.map_error ~f:(fun err ->
-         ( ExitCode 3,
+         ( ErrInternal,
            sprintf "Frontend error:\n%s"
              Frontend.(
                err |> function
@@ -35,7 +50,7 @@ let process_pq_file (filename : string) : (Program.plain_t, error_exit) Result.t
     ~get_source_position:(function First v -> Some v | Second v -> Some v)
     prog
   |> Result.map_error ~f:(fun err ->
-         ( ExitCode 3,
+         ( ErrInternal,
            sprintf "Typing error:\n%s" (TypeChecker.TypingError.print err) ))
   >>= fun tp ->
   let prog_for_quotient_type_checking =
@@ -47,14 +62,14 @@ let process_pq_file (filename : string) : (Program.plain_t, error_exit) Result.t
   in
   QuotientTypeChecker.check_program prog_for_quotient_type_checking
   |> Result.map_error ~f:(fun err ->
-         ( ExitCode 3,
+         ( ErrInternal,
            sprintf "Quotient type checking error:\n%s"
              (err |> QuotientTypeChecker.sexp_of_quotient_typing_error
             |> Sexp.to_string_hum) ))
   >>= function
   | Error err ->
       Error
-        ( ExitCode 2,
+        ( ErrQuotientTypeChecking,
           sprintf "Quotient type check failed:\n%s"
             (QuotientTypeChecker.QuotientTypeCheckingFailure.print err) )
   | Ok () -> Ok (prog |> Program.to_plain_t)
@@ -70,7 +85,7 @@ let write_outputs (input_path : string) (outputs : OcamlConverter.StdM.output) :
       output_string out_file contents;
       close out_file;
       Ok ()
-    with Sys_error msg -> Error (ExitCode 1, msg)
+    with Sys_error msg -> Error (ErrCallingSystemFunctions, msg)
   in
   write_output (pq_filename_to_out_ml_filename input_path) outputs.ml_source
   >>= fun () ->
@@ -83,20 +98,26 @@ let compile_file (input_path : string) : (unit, error_exit) Result.t =
   let output = OcamlConverter.program_to_ocaml prog in
   write_outputs input_path output
 
-let () =
-  Sys.get_argv () |> function
-  | [| _; input_path |] -> (
-      compile_file input_path |> function
-      | Ok () -> ()
-      | Error (ExitCode exit_code, err_msg) ->
-          eprintf "Error:\n%s\n" err_msg;
-          exit exit_code)
-  | _ ->
-      eprintf
-        {|Usage: pq_to_ocaml <input_file>
+let run_main (input_path : string) : int =
+  match compile_file input_path with
+  | Ok () -> Cmd.Exit.ok
+  | Error (e, err_msg) ->
+      eprintf "Error:\n%s\n" err_msg;
+      exit_code e
 
-Exit Codes:
-  0 - All fine
-  1 - Error in calling system functions
-  2 - Quotient typing error
-  3 - Other internal error|}
+let file_path_t : string Term.t =
+  let doc = "Path to the file containing the PQ program" in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
+
+let compile_t : int Term.t = Term.(const run_main $ file_path_t)
+
+let get_exits () : Cmd.Exit.info list =
+  Cmd.Exit.defaults
+  @ List.map ~f:exit_info
+      [ ErrCallingSystemFunctions; ErrQuotientTypeChecking; ErrInternal ]
+
+let compile_info : Cmd.info =
+  let doc = "Compile PQ source code to an OCaml module" in
+  Cmd.info ~doc ~exits:(get_exits ()) "pq_to_ocaml"
+
+let () = Cmd.(Core.exit @@ eval' (v compile_info compile_t))
