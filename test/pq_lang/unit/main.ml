15cb39497a0730c55b6f7f4a04ae1696cb23aefa
Ofsouzap <ofsouzap@gmail.com>
2025-04-14 20:16:43 +0100

    Add flat pattern test script to build

--- test/pq_lang/unit/main.ml
+++ test/pq_lang/unit/main.ml
@@ -5,6 +5,7 @@ let () =
       ("Utils", Test_utils.suite);
       ("Expr", Test_expr.suite);
       ("Program", Test_program.suite);
+      ("Flat Pattern", Test_flatPattern.suite);
       ("Lexer/parser", Test_lexer_parser.suite);
       ("Program Executor", Test_programExecutor.suite);
       ("Program Executor Store", Test_programExecutor_store.suite);
