c961b178b8dae3d1564cd9b18a33435b69118d3b
Ofsouzap <ofsouzap@gmail.com>
2025-04-19 21:18:00 +0100

    Add print_errors parameter to pq_intf functions

--- evaluation/eval_lib/pq_intf.py
+++ evaluation/eval_lib/pq_intf.py
@@ -6,6 +6,8 @@ def generate_pq_source(
     max_variant_types: int = 5,
     max_variant_type_constructors: int = 5,
     max_top_level_defns: int = 5,
+    *,
+    print_errors: bool = False,
 ):
     cmd = [
         "opam",
@@ -26,14 +28,48 @@ def generate_pq_source(
         str(max_top_level_defns),
     ]
 
-    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as e:
+        if print_errors:
+            print(
+                f"""\
+=====Error=====
+Return Code: {e.returncode}
+Command: {e.cmd}
+Output: {e.output}
+Stderr: {e.stderr}
+===============\
+"""
+            )
+        raise e
+
     return result.stdout.strip()
 
 
-def exec_pq_string(program_str: str):
+def exec_pq_string(program_str: str, *, print_errors: bool = False):
     cmd = ["opam", "exec", "--", "dune", "exec", "pq_exec"]
 
-    result = subprocess.run(
-        cmd, input=program_str, capture_output=True, text=True, check=True
-    )
+    try:
+        result = subprocess.run(
+            cmd, input=program_str, capture_output=True, text=True, check=True
+        )
+    except subprocess.CalledProcessError as e:
+        if print_errors:
+            print(
+                f"""\
+=====Error=====
+Return Code: {e.returncode}
+Command: {e.cmd}
+---------------
+Output:
+{e.output}
+---------------
+Stderr:
+{e.stderr}
+===============\
+"""
+            )
+
+        raise e
     return result.stdout.strip()
